d51530ed148d3435912d2fc6951519f15a41c77b
Adison Lampert <adisonlampert@gmail.com>
2023-11-16 15:46:21 -0500

    wip

--- simplified_player.py
+++ simplified_player.py
@@ -1,6 +1,7 @@
 from player import Player
 from itertools import combinations, permutations
 from constants import Orientation
+from tile import Tile
 
 class SimplifiedPlayer(Player):
   def __init__(self):
@@ -26,32 +27,45 @@ class SimplifiedPlayer(Player):
         for i in range(1, len(play_space) - 1):
 
           temp_list = play_space.copy()
-          temp_list[i] = "="
+          temp_list[i] = Tile("=", 0)
 
           for length in range(3, len(play_space) + 1):
               for start in range(len(play_space) - length + 1):
                   end = start + length
                   sliced_list = temp_list[start:end]
+                  
+                  eq, tt = False, False
+                  for t in sliced_list:
+                    if t != None:
+                      if t.getValue() == "=":
+                        eq = True
+                      elif t.getValue() == tile.getValue():
+                        tt = True
+                      
+                      if tt and eq:
+                        break
+                    
+                    if not eq or not tt or sliced_list[0].getValue() == "=" or sliced_list[-1].getValue() == "=":
+                      break
 
-                  if "=" in sliced_list and tile.getValue() in sliced_list and sliced_list[0] != "=" and sliced_list[-1] != "=":
-                      # find all indices to permute over
-                      none_indices = [index for index, value in enumerate(sliced_list) if value is None]
-                      for r in range(1, len(self.rack) + 1):
-                          for comb in combinations(self.rack, r):
-                              for perm in permutations(comb, r):
-                                  temp_sliced_list = sliced_list.copy()
-                                  for idx, val in zip(none_indices, perm):
-                                      temp_sliced_list[idx] = val
-                                  # exclude any equations that have None in between two non-None values
-                                  if all(temp_sliced_list.index(None) in [0, len(temp_sliced_list) - 1] for _ in range(temp_sliced_list.count(None))):
-                                    temp_score = 0
-                                    for j in range(len(temp_sliced_list)):
-                                        if temp_sliced_list[j] is not None:
-                                            temp_score += temp_sliced_list[j].getPoints()
-                                    if temp_score > highest_score:
-                                        if super().validatePlay(temp_sliced_list):
-                                          highest_score = temp_score
-                                          highest_equation = temp_sliced_list
+                    # find all indices to permute over
+                    none_indices = [index for index, value in enumerate(sliced_list) if value is None]
+                    for r in range(1, len(self.rack) + 1):
+                        for comb in combinations(self.rack, r):
+                            for perm in permutations(comb, r):
+                                temp_sliced_list = sliced_list.copy()
+                                for idx, val in zip(none_indices, perm):
+                                    temp_sliced_list[idx] = val
+                                # exclude any equations that have None in between two non-None values
+                                if all(temp_sliced_list.index(None) in [0, len(temp_sliced_list) - 1] for _ in range(temp_sliced_list.count(None))):
+                                  temp_score = 0
+                                  for j in range(len(temp_sliced_list)):
+                                      if temp_sliced_list[j] is not None:
+                                          temp_score += temp_sliced_list[j].getPoints()
+                                  if temp_score > highest_score:
+                                      if super().validatePlay(temp_sliced_list):
+                                        highest_score = temp_score
+                                        highest_equation = temp_sliced_list
 
     for i in range(len(highest_equation)):
       if highest_equation[i] is not None:
@@ -83,7 +97,7 @@ class SimplifiedPlayer(Player):
     playSpace = []
     for i in range(tile.getBefore()):
       playSpace.append(None)
-    playSpace.append(tile.getValue())
+    playSpace.append(tile)
     for i in range(tile.getAfter()):
       playSpace.append(None)
     return playSpace
